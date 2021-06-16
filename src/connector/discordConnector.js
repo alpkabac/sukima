cc9e81a69b10ef83147c3f3690f989329989e773
Noli <nolialsea@gmail.com>
2021-06-16 02:26:00 +0200

    Improve commands

--- src/connector/discordConnector.js
+++ src/connector/discordConnector.js
@@ -45,21 +45,31 @@ bot.on('message', async msg => {
         return
     }
 
+    const msgContent = msg.cleanContent
+
+    if (msgContent.startsWith("²") && msgContent.length === 1) {
+        await msg.edit("`²` command found, retrying...")
+    } else if (msgContent.startsWith(",") && msgContent.length === 1) {
+        await msg.edit("`,` command found, continuing...")
+    } else if (msgContent.startsWith("?") && msgContent.length === 1) {
+        await msg.edit("`?` command found, replying...")
+    }
+
     locked = true
     const message = await botService.onChannelMessage(
         msg.author.username,
         "#" + msg.channel.name,
-        msg.cleanContent,
+        msgContent,
         process.env.BOTNAME)
     locked = false
     if (message && message.message && message.message.trim().length > 0) {
-        if (msg.cleanContent.startsWith("²") && msg.cleanContent.length === 1) {
+        if (msgContent.startsWith("²") && msgContent.length === 1) {
             channels["#" + msg.channel.name].lastBotMessage.edit(message.message)
             msg.delete()
-        } else if (msg.cleanContent.startsWith(",") && msg.cleanContent.length === 1) {
+        } else if (msgContent.startsWith(",") && msgContent.length === 1) {
             channels["#" + msg.channel.name].lastBotMessage.edit(channels["#" + msg.channel.name].lastBotMessage.cleanContent + message.message)
             msg.delete()
-        } else if (msg.cleanContent.startsWith("?") && msg.cleanContent.length === 1) {
+        } else if (msgContent.startsWith("?") && msgContent.length === 1) {
             await msg.channel.send(message.message)
             msg.delete()
         } else {
