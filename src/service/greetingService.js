9ed9ea0da2f8d58109f021e8ab3c9982cef3affd
Noli <nolialsea@gmail.com>
2021-12-17 17:14:42 +0100

    improve greeting prompt

--- src/service/greetingService.js
+++ src/service/greetingService.js
@@ -7,10 +7,11 @@ const greetingGenerator = utils.load("./data/generationPrompt/greetingGeneratorF
 
 class GreetingService {
     static getPrompt(username, botName) {
-        return greetingGenerator
-            .map(m => `${m.from.replace('${botName}', botName)}: ${m.msg}`)
-            .join('\n')
-         + `\nSERVER MESSAGE: User ${username} joined the discord server!\n${botName}:`
+        const context = `[ ${botname} is the official greeting bot of the discord server. ${botName}'s only goal is to greet every new member in the discord server and guide them towards the main discord channels ]\n`
+        return context + greetingGenerator
+                .map(m => `${m.from.replace('${botName}', botName)}: ${m.msg}`)
+                .join('\n')
+            + `\nSERVER MESSAGE: User ${username} joined the discord server!\n${botName}:`
     }
 }
 
