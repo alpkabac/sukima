fda9ce8cd4fb3ee929e3c4f35c7b7b06e8774e08
Noli <nolialsea@gmail.com>
2022-03-13 20:52:32 +0100

    update

--- src/connector/discordConnector.js
+++ src/connector/discordConnector.js
@@ -149,7 +149,7 @@ bot.on('ready', async () => {
             }
         }
         if (connection) {
-            await utils.tts(connection, msg, channelBotTranslationService.getChannelPersonality(channel).voice)
+            await utils.tts2(connection, msg, channelBotTranslationService.getChannelPersonality(channel).voice)
         } else {
             console.log("Could not establish TTS connection.")
         }
