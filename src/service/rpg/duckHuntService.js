42d98d072715a6f5719b3d719c2730cb205b88ad
Noli <nolialsea@gmail.com>
2022-03-17 20:59:22 +0100

    update

--- src/service/rpg/duckHuntService.js
+++ src/service/rpg/duckHuntService.js
@@ -1746,7 +1746,7 @@ class DuckHuntService {
                 + players[playerName].inventory
                     .filter(item => item.forSale)
                     .map(item =>
-                        `${players[playerName].inventory.indexOf(item)}: [${item.rarity} ${item.type}] "${item.name}"${item.image ? ' :white_check_mark:' : ''}${item.forSale ? ` (${item.playerPrice} ${rpgService.getCurrency()})` : ''}`
+                        `Slot [${players[playerName].inventory.indexOf(item)}]: [${item.rarity} ${item.type}] "${item.name}"${item.image ? ' :white_check_mark:' : ''}${item.forSale ? ` (${item.playerPrice} ${rpgService.getCurrency()})` : ''}`
                     )
                     .join('\n')
             )
